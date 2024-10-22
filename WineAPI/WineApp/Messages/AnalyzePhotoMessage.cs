179f3c303334ff4211eb86b8f70eca99d77e9bc7
FlorisVanRooy <r0929450@student.thomasmore.be>
2024-10-22 14:20:44 +0200

    messages

---
+++ WineAPI/WineApp/Messages/AnalyzePhotoMessage.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace WineApp.Messages
+{
+    public class AnalyzePhotoMessage(ImageSource photo) : ValueChangedMessage<ImageSource>(photo)
+    {
+    }
+}
--- WineAPI/WineApp/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/WineApp.AssemblyInfo.cs
+++ WineAPI/WineApp/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/WineApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WineApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8152cca00ab2dab0a8b56f8682e9fc584a169516")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+926dcc3e4cac03197c6521f73b8bebca5f3eb577")]
 [assembly: System.Reflection.AssemblyProductAttribute("WineApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WineApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
