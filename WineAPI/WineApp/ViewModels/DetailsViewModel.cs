6927bb10deed54f3e57d93a5c2286d14c3c7bbbf
Siebe Gios <r0929434@student.thomasmore.be>
2024-10-22 14:20:35 +0200

    push

---
+++ WineAPI/WineApp/ViewModels/DetailsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WineCode.Models;
+
+namespace WineApp.ViewModels
+{
+    public class DetailsViewModel : ObservableObject, IDetailsViewModel
+    {
+        private Recipe recipe;
+
+        public Recipe DetectedRecipe
+        {
+            get { return recipe; }
+            set => SetProperty(ref recipe, value);
+        }
+
+        public ICommand ToggleFavoriteCommand { get; set; }
+        public ICommand ShowMoreWinesCommand { get; set; }
+        public ICommand NavigateToWineCommand { get; set; }
+
+
+    }
+}
+
